3efc7abc604957b32dd9db936be2819229edc04b
needhourger <841904608@qq.com>
2020-05-11 21:01:03 +0800

    rebuild linux to py3

--- linux/linux_wifi.py
+++ linux/linux_wifi.py
@@ -7,95 +7,85 @@ import time
 
 
 url="http://a.nuist.edu.cn/index.php/index/login"
-global data
+file_path=os.path.dirname(__file__)
+print(file_path)
+data={'username':"",'password':"",'enablemacauth':'0',"domain":"NUIST"}
+domain_codes={
+    "1":"NUIST",
+    "2":"CMCC",
+    "3":"ChinaNet",
+    "4":"Unicom"
+}
 
-file_path=os.path.dirname(os.path.abspath(__file__))
-
-def print_logo():
-    os.system("clear")
-    print"  ____  _               _     _          _  __ _ "
-    print" / ___|| |_ _   _ _ __ (_) __| |_      _(_)/ _(_)"
-    print" \___ \| __| | | | '_ \| |/ _` \ \ /\ / / | |_| |"
-    print"  ___) | |_| |_| | |_) | | (_| |\ V  V /| |  _| |"
-    print" |____/ \__|\__,_| .__/|_|\__,_( )_/\_/ |_|_| |_|"
-    print"                 |_|           |/                "
-    print"                          --code by cc|needhourger"
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print(u" 修改账户数据请删除本软件同目录下的config文件  ")
-    print("")
+class ByteEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return obj.decode("utf-8")
+        return json.JSONEncoder.default(self, obj)
 
 
-def First_step():
-    x=""
-    username=""
-    password=""
-    data={}
+def logo():
+    print("""
+      ____  _               _     _          _  __ _ 
+     / ___|| |_ _   _ _ __ (_) __| |_      _(_)/ _(_)
+     \___ \| __| | | | '_ \| |/ _` \ \ /\ / / | |_| |
+      ___) | |_| |_| | |_) | | (_| |\ V  V /| |  _| |
+     |____/ \__|\__,_| .__/|_|\__,_( )_/\_/ |_|_| |_|
+                     |_|           |/                
+                              --code by cc|needhourger
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         修改账户数据请删除本软件同目录下的config文件  
+    """)
 
-    print u"请输入您的账户信息:"
 
-    print u"用户名:",
-    username=raw_input()
+def inputData():
+    global data
 
-    print u"请选择你的认证域(Default:1)：\n 1.南京信息工程大学\n 2.中国移动\n 3.中国电信\n 4.中国联通"
-    print u"请输入你的选择:",
-    x=raw_input()
+    print("请输入您的账户信息:")
+    username=input("用户名 ")
+    password=input("密  码 ")
+    password=base64.b64encode(password.encode("utf-8"))
 
-    print u"密码：",
-    temp=raw_input()
-    password=base64.b64encode(temp)
+    print("请选择你的认证域(Default:1)：\n 1.南京信息工程大学\n 2.中国移动\n 3.中国电信\n 4.中国联通")
+    x=input("请输入你的选择:")
 
     data={'username':username,'password':password,'enablemacauth':'0'}
+    data["domain"]=domain_codes.get(x,"NUIST")
 
-    if x=="":
-        x="1"
-    if x[0]=='1':
-        data['domain']='NUIST'
-    elif x[0]=='2':
-        data['domain']='CMCC'
-    elif x[0]=='3':
-        data['domain']='ChinaNet'
-    elif x[0]=='4':
-        data['domain']='Unicom'
-    else:
-        print u"读入错误"
-        time.sleep(3)
-        exit()
+    if authWifi():
+        saveData()
 
-    with open(file_path+"/config.json","w+") as f:
-        json.dump(data,f)
-        f.close()
-        print u"数据已保存"
-        time.sleep(1.5)
+def saveData():
+    global data
+    with open(os.path.join(file_path,"data.json"),"w",encoding="utf-8") as f:
+        json.dump(data,f,cls=ByteEncoder)
 
-def web_authentication():
-    #print(data)
-    #print(url)
-    while True:
-        try:
-            print_logo()
-            print("tring web authentication...")
-            print("")
-            r=requests.post(url,data=data)
-            break
-        except:
-            continue
-        
-    info=r.json()
-    for key in info:
-        print key,':',info[key]
 
-#name=""
+def loadData():
+    global data
+    if not os.path.exists(os.path.join(file_path,"data.json")):
+        return False
+    with open(os.path.join(file_path,"data.json"),"r",encoding="utf-8") as f:
+        data=json.load(f)
+        return True
+    return False
+
+def authWifi():
+    global data
+    r=requests.post(url,data=data)
+    res=json.loads(r.text)
+    for k,v in res.items():
+        print("\t{}:{}".format(k,v))
+    if res.get("status",0)==1:
+        return True
+    return False
+
+
 if  __name__== "__main__":
-    print_logo()
-    try:
-        with open(file_path+"/config.json","r") as f:
-            data=json.load(f)
-            f.close()
-    except:
-        First_step()
-    finally:
-        print_logo()
-        with open(file_path+"/config.json","r") as f:
-            data=json.load(f)
-            f.close()
-            web_authentication()
+    logo()
+    if not loadData():
+        inputData()
+    else:
+        authWifi()
+    
+
